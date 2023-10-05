b489b5d514b3d078161b204252b55cd2b6dcde2d
unknown <L221050161@pabellon.tecnm.mx>
2023-10-05 09:51:01 -0600

    actualizacion de listas

--- unidad2/python/listas.py
+++ unidad2/python/listas.py
@@ -1,1 +1,27 @@
+
+#listas pracricas 1
 itic3 = ["eduardo flores", "rainer gael rivera rodriguez", "maria del rosario"]
+Carreas_Itpa = ["logistica", "ITICC", "Gestion epresarial", "Industrial", ""]
+Edades = [18, 18, 19, 20, 20, 19, 19, 18, 19, 19, 19, 19, 32]
+
+#imprimir lista
+print(Carreas_Itpa)
+print(itic3)
+print(Edades)
+
+edl Edades[0]
+print(Edades)
+
+print(len(Edades))
+print(len(itic3))
+print(len(Carreas_Itpa))
+
+
+print(Carreas_Itpa[2])
+
+Edades.remove(32)
+print(Edades)
+print("------------------------------------------------------------------")
+print(Edades)
+Edades.sort()
+print(Edades)
